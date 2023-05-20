f885d5089c0c3b58bb482701ef3bd0f9268ee0ed
Matthew Sells <matthew.sells@proton.me>
2023-05-19 19:23:58 -0500

    [sc-187] add base router

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import Koa from 'koa';
+import Router from '@koa/router';
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx) => {
-  ctx.body = 'Hello World';
+router.get('/', (ctx) => {
+  ctx.body = { succes: true };
 });
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000);
