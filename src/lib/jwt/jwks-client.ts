75ca1c5bc4984d9978d6dd84a8d9e7e296eeac7b
Matthew Sells <matthew.sells@proton.me>
2023-07-25 07:05:15 -0500

    fix import

--- src/lib/jwt/jwks-client.ts
+++ src/lib/jwt/jwks-client.ts
@@ -1,6 +1,6 @@
 import createClient from 'jwks-rsa';
 
-import { ENV } from '../application';
+import { ENV } from '../application/env';
 
 export const jwksClient = createClient({
 	jwksUri: ENV.fetch('JWKS_URI'),
