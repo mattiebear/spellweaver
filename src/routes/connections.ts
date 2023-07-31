6c8ef230ea4df8b2c17059629b03bf75283b9ccf
Matthew Sells <matthew.sells@proton.me>
2023-07-30 21:15:45 -0500

    add connection index

--- src/routes/connections.ts
+++ src/routes/connections.ts
@@ -1,12 +1,9 @@
 import Router from '@koa/router';
+import { Not } from 'typeorm';
 
-import { authorize } from '../auth';
-import { mapRepository } from '../db';
-import { Map } from '../entity';
-import { Action } from '../lib/auth';
-import { HttpError, HttpStatus } from '../lib/http';
-import { Hydrator } from '../lib/hydrator';
-import { validate } from '../lib/validation/validate';
+import { connectionRepository } from '../db/connection-repositoy';
+import { ConnectionStatus } from '../entity';
+import { HttpStatus } from '../lib/http';
 import { RequestConnectionService } from '../services/connections/request-connection-service';
 import { AppState } from '../types/state';
 
@@ -16,7 +13,6 @@ interface ConnectionRequestCreateBody {
 	username: string;
 }
 
-// POST a connection/request by searching for a user via username
 router.post('/request', async (ctx) => {
 	const service = new RequestConnectionService({
 		userId: ctx.state.userId,
@@ -28,69 +24,13 @@ router.post('/request', async (ctx) => {
 	ctx.body = service.result();
 	ctx.status = HttpStatus.Created;
 });
-//
-// router.get<AppState>('/', async (ctx) => {
-// 	ctx.body = await mapRepository.findBy({ userId: ctx.state.userId });
-// 	ctx.status = HttpStatus.OK;
-// });
-//
-// // GET map by id
-// router.get<AppState, { id: string }>('/:id', async (ctx) => {
-// 	const map = await mapRepository.findOneBy({
-// 		id: ctx.params.id,
-// 	});
-//
-// 	if (!map) {
-// 		throw new HttpError(HttpStatus.NotFound);
-// 	}
-//
-// 	await authorize(Action.Read, map, ctx.state.userId);
-//
-// 	ctx.body = map;
-// 	ctx.status = HttpStatus.OK;
-// });
-//
-// interface MapPutBody {
-// 	atlas?: object;
-// 	name?: string;
-// }
-//
-// interface MapPatchBody extends Partial<MapPutBody> {}
-//
-// router.patch<AppState, { id: string }, MapPatchBody>('/:id', async (ctx) => {
-// 	const record = await mapRepository.findOneBy({
-// 		id: ctx.params.id,
-// 	});
-//
-// 	if (!record) {
-// 		throw new HttpError(HttpStatus.NotFound);
-// 	}
-//
-// 	await authorize(Action.Update, record, ctx.state.userId);
-//
-// 	const map = new Hydrator(ctx).to(record).hydrates('atlas', 'name').entity();
-//
-// 	await validate(map);
-//
-// 	ctx.body = await mapRepository.save(map);
-// 	ctx.status = HttpStatus.OK;
-// });
-//
-// router.delete<AppState, { id: string }>('/:id', async (ctx) => {
-// 	const map = await mapRepository.findOneBy({
-// 		userId: ctx.state.userId,
-// 		id: ctx.params.id,
-// 	});
-//
-// 	await authorize(Action.Delete, map, ctx.state.userId);
-//
-// 	if (!map) {
-// 		throw new HttpError(HttpStatus.NotFound);
-// 	}
-//
-// 	await mapRepository.remove(map);
-//
-// 	ctx.status = HttpStatus.NoContent;
-// });
+
+router.get<AppState>('/', async (ctx) => {
+	ctx.body = await connectionRepository.findBy({
+		status: Not(ConnectionStatus.Rejected),
+		userId: ctx.state.userId,
+	});
+	ctx.status = HttpStatus.OK;
+});
 
 export { router };
