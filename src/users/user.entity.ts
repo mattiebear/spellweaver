7b467f467fa0472aa91ed2dbd4117b0c48d14e96
Matthew Sells <matthew.sells@proton.me>
2023-08-12 10:59:53 -0500

    update connection services

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,8 +1,13 @@
 import { IsString } from 'class-validator';
+import { pick } from 'ramda';
 
 import { IsUserId } from '../common/lib/validation';
 
 export class User {
+	constructor(data: Partial<User> = {}) {
+		Object.assign(this, pick(['id', 'imageUrl', 'username'], data));
+	}
+
 	@IsUserId()
 	id: string;
 
