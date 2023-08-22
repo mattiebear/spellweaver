844c633a8cd07b678c54b050d2b645ee76aa9e67
Matt Sells <13752442+mattiebear@users.noreply.github.com>
2023-08-21 21:27:17 -0500

    feat: [sc-380] GET Scenario Index (#28)
    * [sc-380] add game session index
    
    * [sc-380] add list and filtering

--- app/controllers/game_sessions_controller.rb
+++ app/controllers/game_sessions_controller.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class GameSessionsController < ApplicationController
+  def index
+    game_sessions = policy_scope(GameSession).where(filters)
+
+    render json: GameSessionBlueprint.render(game_sessions)
+  end
+
   def create
     authorize :game_session
 
@@ -11,5 +17,11 @@ class GameSessionsController < ApplicationController
     ).run!
 
     render json: GameSessionBlueprint.render(service.result)
+  end
+
+  private
+
+  def filters
+    Rest::ListFilters.new(params, :status).filters
   end
 end
---
+++ app/lib/rest/list_filters.rb
@@ -0,0 +1,22 @@
+module Rest
+	class ListFilters
+		def initialize(params, *keys)
+			@params = params
+			@keys = keys
+		end
+
+		def filters
+			keys.reduce({}) do |hash, key|
+				if params[key].present?
+					hash.merge(key => params[key])
+				else
+					hash
+				end
+			end
+		end
+
+		private
+
+		attr_reader :params, :keys
+	end
+end
