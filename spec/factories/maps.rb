22835848f6653a0abce64c1d9e2edcdb51808400
Matthew Sells <matthew.sells@proton.me>
2023-11-29 06:52:05 -0600

    [sc-568] fix tests

--- spec/factories/maps.rb
+++ spec/factories/maps.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :map do
-    atlas { { version: '1', data: [] } }
+    atlas { { version: '1', data: {} } }
 
     sequence(:user_id) { |n| "user_#{n}" }
     sequence(:name) { |n| "Map #{n}" }
