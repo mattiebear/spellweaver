525dda4fd977d9db6c81a6765c2e70f3e9458ea2
Matt Sells <13752442+mattiebear@users.noreply.github.com>
2023-11-29 06:53:51 -0600

    feat: [sc-568] Persist Tilemap (#39)
    * [sc-568] update atlas schema
    
    * [sc-568] fix tests

--- app/models/concerns/atlas_validator.rb
+++ app/models/concerns/atlas_validator.rb
@@ -2,7 +2,7 @@
 
 class AtlasValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    return if value.is_a?(Hash) && value['version'].is_a?(String) && value['data'].is_a?(Array)
+    return if value.is_a?(Hash) && value['version'].is_a?(String) && value['data'].is_a?(Hash)
 
     record.errors.add attribute, (options[:message] || 'is not a valid atlas')
   end
