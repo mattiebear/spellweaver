14cb0393518241e51da693c5037e15c1917d9ebb
Matthew Sells <matthew.sells@proton.me>
2023-12-17 11:42:47 -0600

    [sc-626] update spec

--- spec/lib/story/book_spec.rb
+++ spec/lib/story/book_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Story::Book do
   it 'initializes player data from a game session' do
     game_session = create(:game_session)
 
-    book = described_class.new(game_session)
+    book = described_class.new(game_session, build(:user))
 
     expect(book.to_h).to eq({
                               map_id: nil,
