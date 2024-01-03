53be6f22c4c5e152cedb7c18ddad755cc00ed0e1
Matthew Sells <matthew.sells@proton.me>
2024-01-02 20:12:15 -0600

    [sc-639] format files

--- app/lib/game/actions/change_map.rb
+++ app/lib/game/actions/change_map.rb
@@ -7,11 +7,11 @@ module Game
       def execute!
         load_state.bind do |state|
           state.update(map_id: data[:map_id]).bind do |updated|
-						updated.clear_tokens.bind do |cleared|
-							author.apply(cleared).bind do |saved|
-								success(:change_map, map_id: saved.fields[:map_id])
-							end
-						end
+            updated.clear_tokens.bind do |cleared|
+              author.apply(cleared).bind do |saved|
+                success(:change_map, map_id: saved.fields[:map_id])
+              end
+            end
           end
         end
       end
